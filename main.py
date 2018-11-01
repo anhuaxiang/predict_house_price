a68abee89f838df0a53c21f8720dae7bbec5bb8c
anhuaxiang <1342181530@qq.com>
2018-11-01 14:45:23 +0800

    feature and fix：修复'TypeError: fit_transform() takes 2 positional arguments but 3 were given'的bug,bug 原因版本问题,LabelBinarizer新版本接受两个参数，解决方法重新实现自己的MyLableBinarizer; 添加训练，预测，参数调整等功能

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.pipeline import FeatureUnion
 from sklearn.preprocessing import Imputer
 from pandas.plotting import scatter_matrix
+from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import StandardScaler
@@ -21,27 +22,26 @@ from sklearn.model_selection import train_test_split
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.model_selection import StratifiedShuffleSplit
 
-
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
 HOUSING_PATH = "datasets/housing"
 HOUSING_URL = DOWNLOAD_ROOT + HOUSING_PATH + "/housing.tgz"
+rooms_ix, bedrooms_ix, population_ix, household_ix = 3, 4, 5, 6
 
 
 class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
-    rooms_ix, bedrooms_ix, population_ix, household_ix = 3, 4, 5, 6
-
-    def __init__(self, add_bedrooms_per_room=True):
+    def __init__(self, add_bedrooms_per_room=True):  # no *args or **kargs
         self.add_bedrooms_per_room = add_bedrooms_per_room
 
     def fit(self, X, y=None):
-        return self
+        return self  # nothing else to do
 
     def transform(self, X, y=None):
-        rooms_per_household = X[:, self.rooms_ix] / X[:, self.household_ix]
-        population_per_household = X[:, self.population_ix] / X[:, self.household_ix]
+        rooms_per_household = X[:, rooms_ix] / X[:, household_ix]
+        population_per_household = X[:, population_ix] / X[:, household_ix]
         if self.add_bedrooms_per_room:
-            bedrooms_per_room = X[:, self.bedrooms_ix] / X[:, self.rooms_ix]
-            return np.c_[rooms_per_household, population_per_household, bedrooms_per_room]
+            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+            return np.c_[X, rooms_per_household, population_per_household,
+                         bedrooms_per_room]
         else:
             return np.c_[X, rooms_per_household, population_per_household]
 
@@ -55,6 +55,18 @@ class DataFrameSelector(BaseEstimator, TransformerMixin):
 
     def transform(self, X):
         return X[self.attribute_names].values
+
+
+class MyLabelBinarizer(TransformerMixin):
+    def __init__(self, *args, **kwargs):
+        self.encoder = LabelBinarizer(*args, **kwargs)
+
+    def fit(self, x, y=0):
+        self.encoder.fit(x)
+        return self
+
+    def transform(self, x, y=0):
+        return self.encoder.transform(x)
 
 
 def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
@@ -275,7 +287,7 @@ def pipeline_transform(housing):
     ])
     cat_pipeline = Pipeline([
         ('selector', DataFrameSelector(cat_attribs)),
-        ('label_binarizer', LabelBinarizer()),
+        ('label_binarizer', MyLabelBinarizer()),
     ])
     full_pipeline = FeatureUnion(transformer_list=[
         ("num_pipeline", num_pipeline),
@@ -296,15 +308,87 @@ def cleaning_data():
     return housing, housing_prepared, housing_labels, full_pipeline
 
 
+def display_scores(scores):
+    print("scores:", scores)
+    print("mean", scores.mean())
+    print("standard deviation:", scores.std())
+
+
 def train_model():
+    # 线性
     housing, housing_prepared, housing_labels, full_pipeline = cleaning_data()
     linear_reg = LinearRegression()
-    linear_reg.fix(housing_prepared, housing_labels)
+    linear_reg.fit(housing_prepared, housing_labels)
+
+    # 线性预测后5
     some_data = housing.iloc[:5]
     some_labels = housing_labels.iloc[:5]
     some_data_prepared = full_pipeline.transform(some_data)
     print("Predictions:\t", linear_reg.predict(some_data_prepared))
     print("Labels:\t\t", list(some_labels))
+
+    # 线性预测所有
+    housing_predictions = linear_reg.predict(housing_prepared)
+    lin_mse = mean_squared_error(housing_labels, housing_predictions)
+    lin_rmse = np.sqrt(lin_mse)
+    print("LinearRegression", lin_rmse)
+
+    # 决策树
+    from sklearn.tree import DecisionTreeRegressor
+    tree_reg = DecisionTreeRegressor()
+    tree_reg.fit(housing_prepared, housing_labels)
+    housing_predictions = tree_reg.predict(housing_prepared)
+    tree_mse = mean_squared_error(housing_labels, housing_predictions)
+    tree_rmse = np.sqrt(tree_mse)
+    print("DecisionTreeRegressor", tree_rmse)
+
+    # 交叉验证决策树
+    from sklearn.model_selection import cross_val_score
+    score = cross_val_score(tree_reg, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
+    rmse_scores = np.sqrt(-score)
+    display_scores(rmse_scores)
+
+    # 交叉验证线性
+    lin_scores = cross_val_score(linear_reg, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
+    lin_rmse_scores = np.sqrt(-lin_scores)
+    display_scores(lin_rmse_scores)
+
+    from sklearn.ensemble import RandomForestRegressor
+    forest_reg = RandomForestRegressor()
+    forest_reg.fit(housing_prepared, housing_labels)
+    housing_predictions = forest_reg.predict(housing_prepared)
+    forest_mse = mean_squared_error(housing_labels, housing_predictions)
+    forest_rmse = np.sqrt(forest_mse)
+    print(forest_rmse)
+    forest_scores = cross_val_score(forest_reg, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
+    forest_rmse_scores = np.sqrt(-forest_scores)
+    display_scores(forest_rmse_scores)
+
+    # # 模型保存与重载
+    # from sklearn.externals import joblib
+    # joblib.dump(my_model, "my_model.pkl")
+    # my_model_loaded = joblib.load("my_model.pkl")
+
+    # 网格搜索
+    from sklearn.model_selection import GridSearchCV
+    param_grid = [
+        {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
+        {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features':[2, 3, 4]}
+    ]
+    forest_reg = RandomForestRegressor()
+    grid_search = GridSearchCV(forest_reg, param_grid, cv=5, scoring='neg_mean_squared_error')
+    grid_search.fit(housing_prepared, housing_labels)
+    print(grid_search.best_params_)
+    print(grid_search.best_estimator_)
+    cvres = grid_search.cv_results_
+    for mean_score, params in zip(cvres["mean_test_score"], cvres["params"]):
+        print(np.sqrt(-mean_score), params)
+    feature_importances = grid_search.best_estimator_.feature_importances_
+    print(feature_importances)
+    # extra_attribs = ["rooms_per_hhold", "pop_per_hhold", "bedrooms_per_room"]
+    # cat_one_hot_attribs = list(encoder.classes_)
+    # attributes = num_attribs + extra_attribs + cat_one_hot_attribs
+    # print(sorted(zip(feature_importances, attributes), reverse=True))
 
 
 if __name__ == '__main__':
